dea05760a646dc21b1210bac295f03ba7a2760d4
Jaya Singh <jayasinghrajput0404@gmail.com>
2025-11-26 13:13:22 +0530

    2nd commit

--- Abc.java
+++ Abc.java
@@ -2,5 +2,6 @@ public class Abc {
     public static void main(String[] args) {
         System.out.println("Jaya!!!!");
         System.out.println("Vaishnavi");
+        System.out.println("Hello everyone!!");
     }
 }
